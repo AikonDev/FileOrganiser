8ea22b7526b7c38c2ee20e8bb95827c02baab647
Ayaan <ayaan.jaman10@gmail.com>
2025-07-19 14:21:40 +0100

    Update File Organiser Script.py

--- File Organiser/Source Code/File Organiser Script.py
+++ File Organiser/Source Code/File Organiser Script.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import argparse
 
 def create_subfolder(folder_path, subfolder_name):
     subfolder_path = os.path.join(folder_path, subfolder_name)
@@ -22,9 +23,12 @@ def clean_folder(folder_path):
                 print(f"Moved: {filename} -> {subfolder_name}/")
 
 if __name__ == "__main__":
-    folder_path = 'C:\\PLACEHOLDER\\PLACEHOLDER\\PLACEHOLDER' # Change the path to whatever folder you want to sort your files within
-    if os.path.isdir(folder_path):
-        clean_folder(folder_path)
+    parser = argparse.ArgumentParser(description='Organise files by extension type')
+    parser.add_argument('folder_path', help='Path to the folder you want to organise')
+    args = parser.parse_args()
+    
+    if os.path.isdir(args.folder_path):
+        clean_folder(args.folder_path)
         print("Cleaning complete.")
     else:
         print("Invalid folder path. Please ensure the path is correct and try again.")
